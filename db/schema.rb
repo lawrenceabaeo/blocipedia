13159e1ceec882d0d93d2093ec0b20cfde95f06a
Lawrence Abaeo <labaeo@gmail.com>
2013-12-25 22:00:53 -0800

    All users sent home after pressing subscribe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219232719) do
+ActiveRecord::Schema.define(version: 20131226042157) do
 
   create_table "plans", force: true do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20131219232719) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
