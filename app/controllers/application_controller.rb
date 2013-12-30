3fa68d30556bf99759843c2ada51d1d166c665a0
Lawrence Abaeo <labaeo@gmail.com>
2013-12-29 21:56:28 -0800

    Fixed #26. Had to use new strategy for Rails 4, strong params and devise, see url in bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  # https://github.com/plataformatec/devise#strong-parameters
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end
