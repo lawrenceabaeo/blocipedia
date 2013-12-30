c5c38de54854dd5f755d5de9f6edd17791c621ff
Lawrence Abaeo <labaeo@gmail.com>
2013-12-30 14:05:28 -0800

    Removed production migration so dev would not try to run it

--- db/migrate/20131230215718_remove_public_access_from_wiki_in_production.rb
+++
@@ -1,7 +0,0 @@
-class RemovePublicAccessFromWikiInProduction < ActiveRecord::Migration
-  # NOTE: This change is for production only. 
-  # Due to some incompatibilities and odd sequences, dev db and production db are out of sync
-  def change
-    remove_column :wikis, :public_access
-  end
-end
