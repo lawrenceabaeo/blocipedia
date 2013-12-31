ca143d2d1f18cc7c39ecfb500f076ca522880aa2
Lawrence Abaeo <labaeo@gmail.com>
2013-12-31 13:51:11 -0800

    Improved styling a little bit, for wiki submission errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def error_explanation_tag(errors, &block)
-    if errors.any?
-      content_tag :div, capture(&block), class: 'error_explanation'
-    else
-      content_tag :div, capture(&block), class: 'single-wiki' # don't think this gets called ever
-    end
-  end
 end
