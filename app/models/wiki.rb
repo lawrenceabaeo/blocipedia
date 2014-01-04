1215a92447755728b9b864c93cea62dafb8ff280
Lawrence Abaeo <labaeo@gmail.com>
2014-01-03 20:43:39 -0800

    added nested method to wiki model

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -6,4 +6,5 @@ class Wiki < ActiveRecord::Base
   validates_presence_of :user_id
   validates :title, length: {minimum: 3}, presence: true
   validates :body, length: {minimum: 5}, presence: true
+  accepts_nested_attributes_for :collaborators
 end
