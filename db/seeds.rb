e738933fb5948b9a711c2e6b5d4d593272128aad
Lawrence Abaeo <labaeo@gmail.com>
2014-01-05 09:33:03 -0800

    Fixed #22, deleting all plan records before seeding new records.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-first_plan = Plan.create(name: "Five Dollars A Month", price: 5.00)
+puts ""
+puts "Beginning seeds.rb"
+puts "Deleting all records in Plan database"
+Plan.delete_all
+if (Plan.exists?(1) == false)
+  puts "Plan.exists?(1) is now false, as expected"
+  # puts "Plan.first.nil? is true, as expected"
+else
+  puts "Captain, we have a problem..."
+end
+# Note: It is important that the plan's record id is '1', because stripe matches it's plans with the rails record #. 
+first_plan = Plan.create(id: 1, name: "Five Dollars A Month", price: 5.00)
 puts "The first plan was created. "
 puts "The plan id is: #{first_plan[:id]}"
+puts "Finished seeds.rb"
+puts ""
